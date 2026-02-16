6839e833250e797d5a11b0e3b66c552417440400
Rajkumar Math <rajkumarmath21@gmail.com>
2026-02-16 19:39:11 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,21 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+from app.core.config import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+connect_args = {}
+if "postgres" in settings.DATABASE_URL:
+    connect_args = {"sslmode": "require"}
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args=connect_args
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
-
-# Dependency for routes
+# Dependency for FastAPI
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
