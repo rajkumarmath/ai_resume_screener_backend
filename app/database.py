707d5478fb097151a233c79be7dbb8fffae8d7c9
Rajkumar Math <rajkumarmath21@gmail.com>
2026-02-16 19:30:17 +0530

    Refactor database connection to use environment variable

--- app/database.py
+++ app/database.py
@@ -1,15 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
-from sqlalchemy.orm import declarative_base
+import os
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 # Dependency for routes
 def get_db():
@@ -18,3 +18,4 @@ def get_db():
         yield db
     finally:
         db.close()
+
